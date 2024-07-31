4dd22bd03fa2dd8719310c4c40480c4feffd46e5
unknown <stevenalexander0421@gmail.com>
2024-07-31 17:49:33 +1000

    Initial commit

---
+++ src/ToDoItem.java
@@ -0,0 +1,24 @@
+public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
+
+    public ToDoItem(String description)
+    {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public boolean getIsDone()
+    {
+        return isDone;
+    }
+}
--- src/User.java
+++ src/User.java
@@ -1,39 +1,18 @@
 public class User {
-    private String username;
-    private String password;
+    // ... (other fields and methods)
+
+    private ArrayList<ToDoItem> toDoItems;
 
     public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+        // ... (other initializations)
+        this.toDoItems = new ArrayList<>();
     }
 
     /**
-     * Gets the username of the user.
+     * Gets the to-do items of the user.
+     * @return A reference to the list of to-do items, which can be modified from outside the class.
      */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * Gets the password of the user.
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * Sets the username of the user.
-     * @param username The username to set.
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * Sets the password of the user.
-     * @param password The password to set.
-     */
-    public void setPassword(String password) {
-        this.password = password;
+    public ArrayList<ToDoItem> getToDoItems() {
+        return toDoItems;
     }
 }
